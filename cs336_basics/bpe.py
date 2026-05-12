18bbc3468b49f021421d3dedf56d04b9c1e2d68e
Harvey <cheng.bolong@gmail.com>
2026-05-12 11:16:56 -0700

    better chunking + saving checkpoints

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -1,5 +1,7 @@
 from collections import Counter
 from concurrent.futures import ProcessPoolExecutor
+import json
+import os
 
 import regex as re
 from tqdm import tqdm
@@ -9,7 +11,7 @@ from cs336_basics.pretokenization_example import find_chunk_boundaries
 ENCODE_FMT = "utf-8"
 UTF8_VOCAB_SIZE = 256
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-
+CHECKPOINT_ITERS = 2000
 
 def byte_pair_freq_counter(pre_tok_counter: dict[tuple[bytes, ...], int]) -> dict[tuple[bytes, ...], int]:
     counts = Counter()
@@ -45,18 +47,16 @@ def merge_bytes(pre_tok_counter: dict[tuple[bytes, ...], int], pair: tuple[bytes
 
 def process_chunk(args: tuple[str, int, int, list[str]]) -> Counter:
     input_path, start, end, special_tokens = args
+    special_token_pat = "|".join([re.escape(token) for token in special_tokens])
     pre_tok_counter = Counter()
     with open(input_path, "rb") as f:
         f.seek(start)
         chunk = f.read(end - start).decode(ENCODE_FMT, errors="ignore")
         # Run pre-tokenization on your chunk and store the counts for each pre-token
-        escaped_tokens = [re.escape(token) for token in special_tokens]
-        special_token_pat = "|".join(escaped_tokens)
         sub_chunks = re.split(special_token_pat, chunk)
         for sub_chunk in sub_chunks:
-            pre_tokens = re.findall(PAT, sub_chunk)
-            pre_tok_byte_tuple = [tuple(pre_tok.encode(ENCODE_FMT)) for pre_tok in pre_tokens]
-            pre_tok_counter += Counter(pre_tok_byte_tuple)
+            pre_tokens = re.finditer(PAT, sub_chunk) # stream everything
+            pre_tok_counter += Counter(tuple(pre_tok.group(0).encode(ENCODE_FMT, errors="replace")) for pre_tok in pre_tokens)
     return pre_tok_counter
 
 
@@ -66,24 +66,13 @@ def get_pre_token_counter(
 ) -> Counter:
     # pre_tok_counter = Counter()
     with open(input_path, "rb") as f:
-        num_processes = 8
+        num_processes = max(1, os.cpu_count())
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        chunk_size = max(1, (boundaries[-1] - boundaries[0]) // (20 * num_processes))
 
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
-        #     f.seek(start)
-        #     chunk = f.read(end - start).decode(ENCODE_FMT, errors="ignore")
-        #     # Run pre-tokenization on your chunk and store the counts for each pre-token
-        #     escaped_tokens = [re.escape(token) for token in special_tokens]
-        #     special_token_pat = "|".join(escaped_tokens)
-        #     sub_chunks = re.split(special_token_pat, chunk)
-        #     for sub_chunk in sub_chunks:
-        #         pre_tokens = re.findall(PAT, sub_chunk)
-        #         pre_tok_byte_tuple = [tuple(pre_tok.encode(ENCODE_FMT)) for pre_tok in pre_tokens]
-        #         pre_tok_counter += Counter(pre_tok_byte_tuple)
         args = [(input_path, start, end, special_tokens) for start, end in zip(boundaries[:-1], boundaries[1:])]
         with ProcessPoolExecutor(max_workers=num_processes) as executor:
-            counters = list(tqdm(executor.map(process_chunk, args), total=len(args), desc="Pretokenizing"))
+            counters = list(tqdm(executor.map(process_chunk, args, chunksize=chunk_size), total=len(args), desc="Pretokenizing"))
 
     return sum(counters, Counter())
 
@@ -93,13 +82,13 @@ def train_bpe(
     vocab_size: int,
     special_tokens: list[str],
     verbose: bool = False,
+    save_checkpoint: bool = False
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
 
     vocab: dict[int, bytes] = {}
-    if special_tokens:
-        for i, spe_tok in enumerate(special_tokens):
-            vocab[int(UTF8_VOCAB_SIZE + i)] = spe_tok.encode(ENCODE_FMT)
     vocab.update({idx: bytes([idx]) for idx in range(UTF8_VOCAB_SIZE)})
+    if special_tokens:
+        vocab.update({UTF8_VOCAB_SIZE + idx: spe_tok.encode(ENCODE_FMT) for idx, spe_tok in enumerate(special_tokens)})
     merges: list[tuple[bytes, bytes]] = []
 
     pre_tok_counter = get_pre_token_counter(input_path, special_tokens)
@@ -114,5 +103,14 @@ def train_bpe(
         pre_tok_counter = merge_bytes(pre_tok_counter, byte_pair, idx)
         vocab[idx] = vocab[byte_pair[0]] + vocab[byte_pair[1]]
         merges.append((vocab[byte_pair[0]], vocab[byte_pair[1]]))
+
+        if save_checkpoint and i % CHECKPOINT_ITERS == 0:
+            print(f"Saving checkpoint at iteration {i}")
+            with open(f"./data/owt-vocab-checkpoint-{i}.json", "w") as f:
+                json.dump({v.decode(ENCODE_FMT, errors="replace"): k for k, v in vocab.items()}, f, indent=2)
+            with open(f"./data/owt-merges-checkpoint-{i}.txt", "w") as f:
+                for merge in merges:
+                    f.write(f"{merge[0].decode(ENCODE_FMT, errors="replace")} {merge[1].decode(ENCODE_FMT, errors="replace")}\n")
+            
 
     return vocab, merges
