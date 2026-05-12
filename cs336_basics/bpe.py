7297ab69139ba877db5708c86121c09579f33b9b
Harvey <cheng.bolong@gmail.com>
2026-05-11 20:15:14 -0700

    rearrange

---
+++ cs336_basics/bpe.py
@@ -0,0 +1,97 @@
+from collections import Counter
+
+import regex as re
+
+from cs336_basics.pretokenization_example import find_chunk_boundaries
+
+ENCODE_FMT = "utf-8"
+UTF8_VOCAB_SIZE = 256
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+
+def byte_pair_freq_counter(pre_tok_counter: dict[tuple[bytes, ...], int]) -> dict[tuple[bytes, ...], int]:
+    counts = Counter()
+    for pre_tok_tuple, count in pre_tok_counter.items():
+        if len(pre_tok_tuple) == 1:
+            pass
+        for pair in zip(pre_tok_tuple, pre_tok_tuple[1:]):
+            counts[pair] += count
+
+    return counts
+
+
+def merge_bytes(pre_tok_counter: dict[tuple[bytes, ...], int], pair: tuple[bytes, ...], idx: int):
+    out_counter = Counter()
+    for pre_tok_tuple, count in pre_tok_counter.items():
+        if pair not in zip(pre_tok_tuple, pre_tok_tuple[1:]):
+            out_counter[pre_tok_tuple] = count
+        else:
+            out_ids = []
+            i = 0
+            while i < len(pre_tok_tuple):
+                if i < len(pre_tok_tuple) - 1 and pre_tok_tuple[i] == pair[0] and pre_tok_tuple[i + 1] == pair[1]:
+                    out_ids.append(idx)
+                    i += 2
+                else:
+                    out_ids.append(pre_tok_tuple[i])
+                    i += 1
+
+            out_counter[tuple(out_ids)] = count
+
+    return out_counter
+
+
+def get_pre_token_counter(
+    input_path: str,
+    special_tokens: list[str],
+) -> Counter:
+    pre_tok_counter = Counter()
+    with open(input_path, "rb") as f:
+        num_processes = 4
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode(ENCODE_FMT, errors="ignore")
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
+            escaped_tokens = [re.escape(token) for token in special_tokens]
+            special_token_pat = "|".join(escaped_tokens)
+            sub_chunks = re.split(special_token_pat, chunk)
+            for sub_chunk in sub_chunks:
+                pre_tokens = re.findall(PAT, sub_chunk)
+                pre_tok_byte_tuple = [tuple(pre_tok.encode(ENCODE_FMT)) for pre_tok in pre_tokens]
+                pre_tok_counter += Counter(pre_tok_byte_tuple)
+    return pre_tok_counter
+
+
+def train_bpe(
+    input_path: str,
+    vocab_size: int,
+    special_tokens: list[str],
+    verbose: bool = False,
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+
+    vocab: dict[int, bytes] = {}
+    if special_tokens:
+        for i, spe_tok in enumerate(special_tokens):
+            vocab[int(UTF8_VOCAB_SIZE + i)] = spe_tok.encode(ENCODE_FMT)
+    vocab.update({idx: bytes([idx]) for idx in range(UTF8_VOCAB_SIZE)})
+    merges: list[tuple[bytes, bytes]] = []
+
+    pre_tok_counter = get_pre_token_counter(input_path, special_tokens)
+    num_merges = vocab_size - UTF8_VOCAB_SIZE - len(special_tokens)
+
+    for i in range(num_merges):
+        counts = byte_pair_freq_counter(pre_tok_counter)
+        byte_pair = max(counts, key=lambda k: (counts.get(k), (vocab[k[0]], vocab[k[1]])))  # tiebreak with vocab order
+        idx = UTF8_VOCAB_SIZE + len(special_tokens) + i
+        if verbose:
+            print(f"merge ({vocab[byte_pair[0]], vocab[byte_pair[1]]}) -> {idx} (count: {counts.get(byte_pair)})")
+        pre_tok_counter = merge_bytes(pre_tok_counter, byte_pair, idx)
+        vocab[idx] = vocab[byte_pair[0]] + vocab[byte_pair[1]]
+        merges.append((vocab[byte_pair[0]], vocab[byte_pair[1]]))
+
+    return vocab, merges
--- tests/adapters.py
+++ tests/adapters.py
@@ -2,78 +2,14 @@ from __future__ import annotations
 
 import os
 from collections.abc import Iterable
-from collections import Counter
 from typing import IO, Any, BinaryIO
 
 import numpy.typing as npt
-import regex as re
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
-from cs336_basics.pretokenization_example import find_chunk_boundaries
-
-ENCODE_FMT = "utf-8"
-UTF8_VOCAB_SIZE = 256
-PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-
-
-def byte_pair_freq_counter(pre_tok_counter: dict[tuple[bytes, ...], int]) -> dict[tuple[bytes, ...], int]:
-    counts = Counter()
-    for pre_tok_tuple, count in pre_tok_counter.items():
-        if len(pre_tok_tuple) == 1:
-            pass
-        for pair in zip(pre_tok_tuple, pre_tok_tuple[1:]):
-            counts[pair] += count
-
-    return counts
-
-
-def merge_bytes(pre_tok_counter: dict[tuple[bytes, ...], int], pair: tuple[bytes, ...], idx: int):
-    out_counter = Counter()
-    for pre_tok_tuple, count in pre_tok_counter.items():
-        if pair not in zip(pre_tok_tuple, pre_tok_tuple[1:]):
-            out_counter[pre_tok_tuple] = count
-        else:
-            out_ids = []
-            i = 0
-            while i < len(pre_tok_tuple):
-                if i < len(pre_tok_tuple) - 1 and pre_tok_tuple[i] == pair[0] and pre_tok_tuple[i + 1] == pair[1]:
-                    out_ids.append(idx)
-                    i += 2
-                else:
-                    out_ids.append(pre_tok_tuple[i])
-                    i += 1
-
-            out_counter[tuple(out_ids)] = count
-
-    return out_counter
-
-
-def get_pre_token_counter(
-    input_path: str,
-    special_tokens: list[str],
-) -> Counter:
-    pre_tok_counter = Counter()
-    with open(input_path, "rb") as f:
-        num_processes = 4
-        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
-
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
-
-        for start, end in zip(boundaries[:-1], boundaries[1:]):
-            f.seek(start)
-            chunk = f.read(end - start).decode(ENCODE_FMT, errors="ignore")
-            # Run pre-tokenization on your chunk and store the counts for each pre-token
-            escaped_tokens = [re.escape(token) for token in special_tokens]
-            special_token_pat = "|".join(escaped_tokens)
-            sub_chunks = re.split(special_token_pat, chunk)
-            for sub_chunk in sub_chunks:
-                pre_tokens = re.findall(PAT, sub_chunk)
-                pre_tok_byte_tuple = [tuple(pre_tok.encode(ENCODE_FMT)) for pre_tok in pre_tokens]
-                pre_tok_counter += Counter(pre_tok_byte_tuple)
-    return pre_tok_counter
+from cs336_basics.bpe import train_bpe
 
 
 def run_linear(
@@ -655,26 +591,5 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    vocab: dict[int, bytes] = {}
-    if special_tokens:
-        for i, spe_tok in enumerate(special_tokens):
-            vocab[int(UTF8_VOCAB_SIZE + i)] = spe_tok.encode(ENCODE_FMT)
-    vocab.update({idx: bytes([idx]) for idx in range(UTF8_VOCAB_SIZE)})
-    merges: list[tuple[bytes, bytes]] = []
 
-    pre_tok_counter = get_pre_token_counter(input_path, special_tokens)
-    # byte_seq_counter: dict[tuple[bytes, ...], int]
-
-    num_merges = vocab_size - UTF8_VOCAB_SIZE - len(special_tokens)
-
-    for i in range(num_merges):
-        counts = byte_pair_freq_counter(pre_tok_counter)
-        byte_pair = max(counts, key=lambda k: (counts.get(k), (vocab[k[0]], vocab[k[1]])))  # tiebreak with vocab order
-        idx = UTF8_VOCAB_SIZE + len(special_tokens) + i
-        # if verbose:
-        #     print(f"merge ({vocab[byte_pair[0]], vocab[byte_pair[1]]}) -> {idx} (count: {counts.get(byte_pair)})")
-        pre_tok_counter = merge_bytes(pre_tok_counter, byte_pair, idx)
-        vocab[idx] = vocab[byte_pair[0]] + vocab[byte_pair[1]]
-        merges.append((vocab[byte_pair[0]], vocab[byte_pair[1]]))
-
-    return vocab, merges
+    return train_bpe(input_path, vocab_size, special_tokens, **kwargs)
